03fbade31d705397b96b5b86930ef6cb4ab5c670
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-02-27 22:49:30 +0100

    Wrote function for ProTeamListAdapter.java.

--- app/src/main/java/danilchanka/aliaksandr/dota2proteam/adapter/ProTeamListAdapter.java
+++ app/src/main/java/danilchanka/aliaksandr/dota2proteam/adapter/ProTeamListAdapter.java
@@ -1,4 +1,58 @@
 package danilchanka.aliaksandr.dota2proteam.adapter;
 
-public class ProTeamListAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import danilchanka.aliaksandr.dota2proteam.R;
+import danilchanka.aliaksandr.dota2proteam.adapter.base.SimpleRecyclerAdapter;
+import danilchanka.aliaksandr.dota2proteam.entity.ProTeam;
+
+public class ProTeamListAdapter extends SimpleRecyclerAdapter<ProTeam> {
+
+    @Inject
+    public ProTeamListAdapter(Context context) {
+        super(context);
+    }
+
+    @Override
+    public SimpleRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ProTeamViewHolder(getContext(), getViewHolderView(parent, R.layout.row_pro_team));
+    }
+
+    public class ProTeamViewHolder extends SimpleRecyclerViewHolder<ProTeam> {
+        @BindView(R.id.imgUserIcon)
+        ImageView mImgUserIcon;
+        @BindView(R.id.layoutContent)
+        RelativeLayout mRelativeLayout;
+        @BindView(R.id.txtName)
+        TextView mTxtName;
+        @BindView(R.id.txtRating)
+        TextView mTxtPhone;
+
+        public ProTeamViewHolder(Context context, View itemView) {
+            super(context, itemView);
+        }
+
+        @Override
+        protected void bind(ProTeam proTeam) {
+            super.bind(proTeam);
+            mTxtName.setText(proTeam.getName());
+            mTxtPhone.setText(String.format("%s", proTeam.getRating()));
+
+            if (proTeam.getLogo_url() != null) {
+                Picasso.with(mRelativeLayout.getContext())
+                        .load(proTeam.getLogo_url())
+                        .into(mImgUserIcon);
+            }
+        }
+    }
 }
