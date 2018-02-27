c256b38f9d0b59a95bcde029f44cddd60e5bd7f0
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-02-27 13:51:18 +0100

    Add function in ProTeamListMvpView.java for work with data.

--- app/src/main/java/danilchanka/aliaksandr/dota2proteam/view/ProTeamListMvpView.java
+++ app/src/main/java/danilchanka/aliaksandr/dota2proteam/view/ProTeamListMvpView.java
@@ -2,6 +2,19 @@ package danilchanka.aliaksandr.dota2proteam.view;
 
 import com.arellomobile.mvp.MvpView;
 
+import java.util.ArrayList;
+
+import danilchanka.aliaksandr.dota2proteam.entity.ProTeam;
+
 public interface ProTeamListMvpView extends MvpView {
 
+    void showProTeamList(ArrayList<ProTeam> proTeamList);
+
+    void showLoading();
+
+    void showEmpty();
+
+    void showError();
+
+    void showRefreshingError();
 }
