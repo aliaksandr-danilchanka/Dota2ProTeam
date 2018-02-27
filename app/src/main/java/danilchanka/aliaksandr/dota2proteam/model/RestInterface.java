5ea61884814255282cbff2de59a81df5ce7dd3c3
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-02-27 22:50:53 +0100

    Changed libraries.

--- app/src/main/java/danilchanka/aliaksandr/dota2proteam/model/RestInterface.java
+++ app/src/main/java/danilchanka/aliaksandr/dota2proteam/model/RestInterface.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 import danilchanka.aliaksandr.dota2proteam.entity.ProTeam;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
-import rx.Observable;
-import rx.Single;
+import io.reactivex.Observable;
 
 public interface RestInterface {
     @GET("teams")
     Observable<ArrayList<ProTeam>> getProTeamList();
 
     @GET("teams/{team_id}")
-    Single<ProTeam> getProTeamDetail(@Path("team_id") String teamId);
+    Observable<ProTeam> getProTeamDetail(@Path("team_id") String teamId);
 }
